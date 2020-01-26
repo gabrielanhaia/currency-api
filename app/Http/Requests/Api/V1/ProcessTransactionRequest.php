bd42fb1f07b538aecaae5854fcb962ba1a420915
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-26 11:46:01 +0000

    feature: Create request validation (v1) to store transactions

---
+++ app/Http/Requests/Api/V1/ProcessTransactionRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class ProcessTransactionRequest Responsible for validating the creation of transactions.
+ * @package App\Http\Requests\Api\V1
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class ProcessTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'usedId' => 'required|integer',
+            'currencyFrom' => 'required|string',
+            'currencyTo' => 'required|string',
+            'amountSell' => 'required|numeric',
+            'amountBuy' => 'required|numeric',
+            'country_origin' => 'required|string'
+        ];
+    }
+}
